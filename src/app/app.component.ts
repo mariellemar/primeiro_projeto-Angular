ee19d91cae64d2aa643e50c366ab1ab1d069db10
Marielle Mar <mariellemarsantos@gmail.com>
2023-06-07 11:52:48 -0400

    angular sessão #5 - diretivas atributos/3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,14 @@ import { Component, OnChanges, OnDestroy, OnInit} from '@angular/core';
   <app-diretivas-estruturais></app-diretivas-estruturais>
   <app-data-biding></app-data-biding> 
   -->
-  <app-diretivas-atributos></app-diretivas-atributos>
+ 
+  
+  <app-diretivas-atributos>
+   <!-- <h1>Aulas de diretivas atributo</h1>
+    <hr> -->
+  </app-diretivas-atributos>
+ 
+
   <router-outlet></router-outlet>`
 })
 
--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -15,6 +15,7 @@ export class DiretivasAtributosComponent implements OnInit {
   public nome: string = "";
   public list: Array <{ nome: string }> =[{nome: "Mari"}, {nome: "Sabrina"}];
 
+  public date: Date = new Date()
   constructor() { }
 
   ngOnInit(): void {
